96d48b23f2100c1f918e6f233c88f379211ce72e
or-forshmit8 <or.forshmit@mail.huji.ac.il>
2024-12-07 04:15:50 +0200

    Fixed Javadoc issues and added Javadoc

--- src/bricker/brick_strategies/DoubleBehaviorStrategy.java
+++ src/bricker/brick_strategies/DoubleBehaviorStrategy.java
@@ -1,5 +1,6 @@
 package bricker.brick_strategies;
 
+import bricker.main.BrickerGameManager;
 import danogl.GameObject;
 
 /**
@@ -9,13 +10,17 @@ public class DoubleBehaviorStrategy implements CollisionStrategy {
 
     private final CollisionStrategy strategy1;
     private final CollisionStrategy strategy2;
+    private final BasicCollisionStrategy basicStrategy;
 
     /**
      * Creates a new DoubleBehaviorStrategy with the given strategies.
      * @param strategy1 The first strategy.
      * @param strategy2 The second strategy.
      */
-    public DoubleBehaviorStrategy(CollisionStrategy strategy1, CollisionStrategy strategy2) {
+    public DoubleBehaviorStrategy(
+            BrickerGameManager gameManager, CollisionStrategy strategy1, CollisionStrategy strategy2
+    ) {
+        this.basicStrategy = new BasicCollisionStrategy(gameManager);
         this.strategy1 = strategy1;
         this.strategy2 = strategy2;
     }
@@ -27,6 +32,8 @@ public class DoubleBehaviorStrategy implements CollisionStrategy {
      */
     @Override
     public void onCollision(GameObject thisObj, GameObject otherObj) {
+        // Make sure to remove the object from the game if it is a brick in case of 3 doubleBehaviorStrategies
+        basicStrategy.onCollision(thisObj, otherObj);
         if (strategy1 != null) {
             strategy1.onCollision(thisObj, otherObj);
         }
